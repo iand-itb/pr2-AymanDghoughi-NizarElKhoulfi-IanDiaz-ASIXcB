d2f1107711f7afce10c5e3fc3773a12da6587586
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-10-11 12:09:35 +0200

    bomboclat

--- HowBigIsMyPizza.py
+++ HowBigIsMyPizza.py
@@ -1,7 +1,7 @@
 '''
 Ayman Dghoughi, Nizar ElKhoulfi, Ian Diaz
 M03 UF1
-Calcular la superfície d'una pizza a partir del diàmetre
+Descripció: Calcular la superfície d'una pizza a partir del diàmetre
 11/10/23
 '''
 
--- WorkingAge.py
+++ WorkingAge.py
@@ -1,7 +1,13 @@
 '''
 Ayman Dghoughi, Nizar ElKhoulfi, Ian Diaz
 M03 UF1
-Llegeixi l'edat de l'usuari i mostri si té edat per treballar
+Descripció: Llegeixi l'edat de l'usuari i mostri si té edat per treballar
 11/10/23
 '''
 
+edat = int(input("Quina edat tens? "))
+
+if edat >= 16 and edat <= 65:
+    print("Apte per treballar")
+else:
+    print("No apte per treballar")
