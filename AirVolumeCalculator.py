23445f7abfa74017d14be5862f48a5df5ccbd512
nizarelkhoulfiitb <nizar.elkhoulfi.itb@itb.cat>
2023-10-11 12:12:23 +0200

    Exercici 2 acabat

--- AirVolumeCalculator.py
+++ AirVolumeCalculator.py
@@ -1,7 +1,18 @@
 '''
 Ayman Dghoughi, Nizar ElKhoulfi, Ian Diaz
 M03 UF1
-Calcular la superfície d'una pizza a partir del diàmetre
+Calcular el volum d'una habitació a partir de les 3 dimensions
 11/10/23
 '''
+
+print("Amplada de l'habitació?")
+amplada = int(input())
+print("Llargada de l'habitació?")
+llargada = int(input())
+print("Alçada de l'habitació?")
+alcada = int(input())
+
+volumen = int(amplada * llargada * alcada)
+
+print("El volum de l'aula és " + str(volumen) + "m³")
 
