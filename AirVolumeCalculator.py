b78345265d1c1a10ff6312c059ccbc6d709988d9
nizarelkhoulfiitb <145555456+nizarelkhoulfiitb@users.noreply.github.com>
2023-10-11 13:04:40 +0200

    Update AirVolumeCalculator.py

--- AirVolumeCalculator.py
+++ AirVolumeCalculator.py
@@ -5,3 +5,24 @@ Calcular la superfície d'una pizza a partir del diàmetre
 11/10/23
 '''
 
+#Demanem al usuari els diferents valors per poder calcular el volum
+print("Amplada de l'habitació?")
+#Agafem el valor i el convertim a float
+amplada = float(input())
+
+#Demanem al usuari els diferents valors per poder calcular el volum
+print("Llargada de l'habitació?")
+#Agafem el valor i el convertim a float
+llargada = float(input())
+
+#Demanem al usuari els diferents valors per poder calcular el volum
+print("Alçada de l'habitació?")
+#Agafem el valor i el convertim a float
+alcada = float(input())
+
+#Fem el calcul amb el valors agafats (apliquem la formula del volum)
+volumen = float(amplada * llargada * alcada)
+
+#Mostrem en pantalla la frase amb el resultat en m³
+print(f"El volum de l'aula és {volumen:.2f} m³")
+
