8605b9ccb1292a22e9087cce09531344fce6d95c
sjo <ian.diaz.7e5@itb.cat>
2023-10-11 11:52:36 +0200

    yepa ya estoy por aqui

---
+++ AirVolumeCalculator.py
@@ -0,0 +1,7 @@
+'''
+Ayman Dghoughi, Nizar ElKhoulfi, Ian Diaz
+M03 UF1
+Calcular la superfície d'una pizza a partir del diàmetre
+11/10/23
+'''
+
---
+++ HowBigIsMyPizza.py
@@ -0,0 +1,15 @@
+'''
+Ayman Dghoughi, Nizar ElKhoulfi, Ian Diaz
+M03 UF1
+Calcular la superfície d'una pizza a partir del diàmetre
+11/10/23
+'''
+
+import math
+
+#Demanem el diàmetre de la pizza com a float.
+diametre = float(input("Introdueix el diàmetre de la pizza en centímetres...\t"))
+
+superficie = math.pi * (diametre**2 / 4)
+
+print(f"\nLa superficie de la pizza es: {superficie:.2f}cm")
