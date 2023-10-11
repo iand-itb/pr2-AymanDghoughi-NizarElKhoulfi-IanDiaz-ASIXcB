18b1f2084d2ae2a88e5e66f6ec2f0cb27e9672a9
sjo <ian.diaz.7e5@itb.cat>
2023-10-11 12:28:22 +0200

    inrange

--- InRange.py
+++ InRange.py
@@ -4,3 +4,10 @@ M03 UF1
 Mostra True si el cinquè valor, està comprès dins els dos rangs
 11/10/23
 '''
+
+valors = list(map(int, input("Introdüeix 5 valors separats per un espai...\n").split(" ")))
+
+if valors[4] in range(valors[0], valors[1]) and valors[4] in range(valors[2], valors[3]):
+    print("True")
+else:
+    print("False")
