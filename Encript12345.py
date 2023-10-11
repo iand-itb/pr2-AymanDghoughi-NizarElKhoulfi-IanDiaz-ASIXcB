117094fe5fa3f7a8a01f6006f662c8149774c3fe
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-10-11 12:27:19 +0200

    bomboclat

---
+++ Encript12345.py
@@ -0,0 +1,16 @@
+'''
+Ayman Dghoughi, Nizar ElKhoulfi, Ian Diaz
+M03 UF1
+Descripció: Llegeixi l'edat de l'usuari i mostri si té edat per treballar
+11/10/23
+'''
+
+un = 1
+dos = 2
+tres = 3
+quatre = 4
+cinc = 5
+
+word = str(input("Tell me a word: "))
+
+if word
