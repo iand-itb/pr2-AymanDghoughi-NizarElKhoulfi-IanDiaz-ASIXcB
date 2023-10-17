4b888503954bb189570156bdc123d4f9cb4d6355
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-10-17 08:46:10 +0200

    flox

--- AirVolumeCalculator.py
+++ AirVolumeCalculator.py
@@ -5,24 +5,16 @@ ASIXc M03 UF1 A2
 Descripció: Programa per calcular la superfície d'una pizza a partir del diàmetre
 '''
 
-#Demanem al usuari els diferents valors per poder calcular el volum
 print("Amplada de l'habitació? ")
-#Agafem el valor i el convertim a float
 amplada = float(input())
 
-#Demanem al usuari els diferents valors per poder calcular el volum
 print("Llargada de l'habitació? ")
-#Agafem el valor i el convertim a float
 llargada = float(input())
 
-#Demanem al usuari els diferents valors per poder calcular el volum
 print("Alçada de l'habitació? ")
-#Agafem el valor i el convertim a float
-alcada = float(input())
+alçada = float(input())
 
-#Fem el calcul amb el valors agafats (apliquem la formula del volum)
-volumen = float(amplada * llargada * alcada)
+volum = float(amplada * llargada * alçada)
 
-#Mostrem en pantalla la frase amb el resultat en m³
-print(f"El volum de l'aula és {volumen:.2f} m³")
+print(f"El volum de l'aula és {volum:.2f} m³")
 
